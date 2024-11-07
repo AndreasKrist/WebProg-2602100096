b30002878139281f0bbc427ffd142519da5b4ef0
Andreas Kristianto <andreaskrist2004@gmail.com>
2024-11-07 23:38:43 +0700

    WebProg 2602100096
    webprog v1

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="container-fluid py-3 bg-dark" style="display: absolute; bottom: 0; left: 0; right: 0;">
+    <p class="text-light text-center">&copy; EduFun 2024 | Web Programming | Andreas Mardohar Kristianto | 2602100096</p>
+</footer>
---
+++ resources/views/menu/about-us.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.body')
+@section('title', 'About Us')
+
+@section('konten')
+    <div class="row">
+        <div class="col-md-8 offset-md-2">
+            <div style="text-align: center;">
+                <h1 style="margin-top: 120px; margin-bottom: 30px;">About EduFun</h1>
+                <p style="font-size: 20px;">EduFun telah lama menjadi bagian dari lembaga pendidikan Indonesia berbasis Teknologi. Kami menawarkan pendidikan Teknologi yang mendalam kepada seluruh masyarakat Indonesia. Koten yang kami punya merupakan hasil kolaborasi dengan dosen hebat di beberapa universitas terkenal.</p>
+                <p style="font-size: 20px;">Kami terus melakukan ekspansi sehingga course yang kami punya semakin banyak dan beragam. Tujuan kami ingnin memberikan pegnetahuan teknologi kepada masyarakat Indonesia, agar lebih meleh dan paham dengan persaingan global mengenai Teknologi. EduFun menginginkan memberikan satu juta programmar Indonesia. Hal ini sejalan dengan prinsip kedepan kita Indonesia Emas 2045</p>
+            </div>
+        </div>
+    </div>
+@endsection
